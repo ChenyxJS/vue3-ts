3c55981c1c2b1d89267a17dba618af1ac477bbd8
mac <2412235462@qq.com>
2023-05-11 15:05:21 +0800

    build: :package: 代码格式文件修改

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,17 +2,17 @@
  * @Author: chenyx
  * @Date: 2023-04-11 18:41:34
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-05-11 12:03:45
- * @FilePath: /vue3-ts-template/src/views/home/index.vue
+ * @LastEditTime: 2023-05-11 15:01:19
+ * @FilePath: /chenyx-cli/src/views/home/index.vue
 -->
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <h1>Hello, Wellcome to Vue3 TypeScript Template</h1>
+    <h1>Hello, Wellcome to Vue3 TypeScript Template test</h1>
 </template>
 
 <style lang="scss" scoped>
-
+h1 {
+    margin: auto;
+}
 </style>
